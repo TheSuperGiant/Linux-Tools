97bd19d8995b7a3ed975bf95dc5cc7e563578b6e
TheSuperGiant <42948694+TheSuperGiant@users.noreply.github.com>
2026-01-12 23:48:56 +0000

    Add files via upload

--- ext4setup.sh
+++ ext4setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/main/functions.sh |
-sed -n '
-	/^error[[:space:]]*()/,/^}/p
-	/^ext4setup[[:space:]]*()/,/^}/p
-'
+source <(
+	curl -s -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/main/functions.sh |
+	sed -n '
+		/^error[[:space:]]*()/,/^}/p
+		/^ext4setup[[:space:]]*()/,/^}/p
+	'
+)
 ext4setup
